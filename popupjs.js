4be952a14bfaf64b3f1d9f32e901d1cac0871145
aminet3 <77569075+aminet3@users.noreply.github.com>
2024-04-19 11:48:36 +0100

    Update popupjs.js

--- popupjs.js
+++ popupjs.js
@@ -1,90 +1,6 @@
 
 
-<div id="MrAminBen_popup"
-    class="FlexContainer-lh6d3k-0 ModalWindow__Window-sc-1rlc96w-0 epHMcu eapp-popup-layout-variation-modal-component eapp-popup-layout-variation-modal-component-enter-done"
-    role="dialog" tabindex="-1" aria-modal="true" style="pointer-events: all;display:none;">
-    <div class="ModalBackdrop__Backdrop-sc-873age-0 envSuS eapp-popup-modal-overlay-component eapp-popup-modal-overlay-component-transition-entered"
-        style="background-color: rgba(17, 17, 17, 0.7);"></div>
-  
-  
-    <div class="FlexItem-rdg44b-0 ModalContent__Content-sc-1lyzazn-0 bzgeF eapp-popup-layout-variation-modal-content-container eapp-popup-layout-variation-modal-content-container-transition-entered"
-        role="document">
-        <div class="FlexContainer-lh6d3k-0 dRKFRl eapp-popup-content-component">
-            <div onclick="MrAminBen_popup()"
-                class="Button__Block-sc-1c0eo6i-0 bhCgZe ModalControl__Control-sc-1dl29es-0 cLmiHt jsx-2881417189 eapp-popup-control-close-component transition-exited"
-                tabindex="0" role="button">
-                <svg viewBox="0 0 10 10" class="eapp-popup-control-close-icon">
-                    <path fill-rule="nonzero"
-                        d="M6.414 5l3.293 3.293a1 1 0 0 1-1.414 1.414L5 6.414 1.707 9.707A1 1 0 1 1 .293 8.293L3.586 5 .293 1.707A1 1 0 0 1 1.707.293L5 3.586 8.293.293a1 1 0 0 1 1.414 1.414L6.414 5z">
-                    </path>
-                </svg>
-            </div>
-            <div class="FlexItem-rdg44b-0 jBExIs">
-                <div class="FlexContainer-lh6d3k-0 jyfuVZ eapp-popup-content-main">
-                    <div class="FlexItem-rdg44b-0 cDvFSS eapp-popup-content-outer"
-                        style="background-image: url('https://i.imgur.com/P5X93wx.png');">
-                        <div class="eapp-popup-content-outer-image-overlay"
-                            style="background-color: rgba(17, 17, 17, 0.6);"></div>
-                        <div class="eapp-popup-content-inner">
-                            <div
-                                class="eapp-popup-content-blocks-container-component eapp-popup-content-blocks-container-paddings-medium">
-                                <div class="eapp-popup-content-blocks-container-item">
-                                    <div
-                                        class="FlexContainer-lh6d3k-0 fWQEQp eapp-popup-block-variation-image-component">
-                                        <div class="FlexContainer-lh6d3k-0 enfMWB">
-                                            <img src="https://files.elfsight.com/storage/4bc9e068-6999-4781-ad0c-ab83bceba5ea/84590d74-76f1-4519-8a17-553116fd9de7.png"
-                                                alt="" class="jsx-3400482160 eapp-popup-block-variation-image-img" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="eapp-popup-content-blocks-container-item">
-                                    <div class="jsx-651102099 eapp-popup-block-variation-spacing-component"></div>
-                                </div>
-                                <div class="eapp-popup-content-blocks-container-item">
-                                    <div class="jsx-711716781 eapp-popup-block-variation-title-component"> عروض موسم الصيف                                   !</div>
-                                </div>
-                                <div class="eapp-popup-content-blocks-container-item">
-                                    <div class="jsx-952473858 eapp-popup-block-variation-title-component"> تخفيضات تصل
-                                        الى 50%-</div>
-                                </div>
-                              
-                              
-                              
-                                <div class="eapp-popup-content-blocks-container-item">
-                                    <div class="jsx-2715126705 eapp-popup-block-variation-text-component">
-                                        <div class="jsx-2715126705 eapp-popup-block-variation-text-text">رقم القسيمة
-                                            :<br /></div>
-                                    </div>
-                                </div>
-                                <div class="eapp-popup-content-blocks-container-item">
-                                    <div class="jsx-2749092036 eapp-popup-block-variation-coupon-component">
-                                        <div class="Button__Block-sc-1c0eo6i-0 bhCgZe jsx-2749092036 eapp-popup-block-variation-coupon-area"
-                                            tabindex="0" role="button">
-                                            <div class="jsx-2749092036 eapp-popup-block-variation-coupon-tooltip">قم
-                                                بنسخ الرمز</div>
-                                            <div class="eapp-popup-block-variation-coupon-code">GET50</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="eapp-popup-content-blocks-container-item">
-                                    <div class="eapp-popup-block-variation-button-component">
-                                        <div></div>
-                                        >
-                                            <a class="Button__Block-sc-1c0eo6i-0 bhCgZe jsx-3403832932 eapp-popup-button-component"
-    tabindex="0" role="button" href="javascript:void(0);" onclick="closePopup()">ابدأ التسوق</a>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Popup  || Coded By MrAminBen -->
-<script> function MrAminBen_popup(){
+ function MrAminBen_popup(){
     document.getElementById("MrAminBen_popup").style.display='none';
 }
 
@@ -135,4 +51,3 @@ function closePopup() {
     }
 }
 
-</script>
